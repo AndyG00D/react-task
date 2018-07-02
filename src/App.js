ea1eeabd3ca1eb02f4dfceea9104845f429bfb57
AndyG00D <achicunov@gmail.com>
2018-07-02 20:12:17 +0300

    +axios

--- src/App.js
+++ src/App.js
@@ -4,20 +4,27 @@ import Header from "./components/Header/Header";
 import Player from "./components/Player/Player";
 import Hello from "./components/Hello/Hello";
 // import Hello from "./components/Hello/Hello";
+import {BrowserRouter, Link, Route} from "react-router-dom"
+
+const Movies = function(){
+  return <h1>Movies</h1>
+};
 
 
 export default function App() {
   return (
+    <BrowserRouter>
     <div className="app">
       <Header/>
       <div className="content">
-        {/*<Hello/>*/}
         {/*<Player/>*/}
         <Hello
           number = {4}
           name = {'Denis'}
         />
+        <Route path="/movies" component={Movies}/>
       </div>
     </div>
+    </BrowserRouter>
   );
 }
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,16 +1,31 @@
 import React from 'react';
-import './Header.css'
+import styles from './Header.css'
+import styled from 'styled-components'
+import {Link} from 'react-router-dom'
+
+const LogoStyled = styled.img`
+
+  float: left;
+  height: 20px;
+
+`;
 
 
 export default function Header() {
   return (
-    <header className="header">
-      <img
+    <header className={styles.header}>
+
+      <ul>
+        <li>home</li>
+        <li> <Link to="/movies"> movies</Link></li>
+        <li>about</li>
+      </ul>
+
+      <LogoStyled
         src="http://pluspng.com/img-png/deezer-png-deezer-1694.png"
-        className="header__logo"
         alt="logo"
       />
-      <h1 className="header__title">React Task</h1>
+      <h1 className={styles.header__title}>React Task</h1>
     </header>
   );
 }
